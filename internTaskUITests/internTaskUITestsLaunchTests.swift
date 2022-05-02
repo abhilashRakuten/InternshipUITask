e02d677ff71b2f0f7de8d23149c65f2ee28dbf64
aaakashsingh <ar-akash.a.singh@rakuten.com>
2022-05-02 16:56:09 +0530

    commit - #1 : Files added

---
+++ internTaskUITests/internTaskUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  internTaskUITestsLaunchTests.swift
+//  internTaskUITests
+//
+//  Created by Singh, Akash | RIEPL on 30/04/22.
+//
+
+import XCTest
+
+class internTaskUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
